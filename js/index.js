0a7f6549533fc51d32cee422fc385d898fb49f38
chenCB931223 <42188252+chenCB931223@users.noreply.github.com>
2018-09-26 08:43:23 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,64 +1,85 @@
 $(function() {
-    $('body').on('click','.view-pic', function() {
-        var $container = $('<div></div>');
-        $container.attr('id', 'view-box');
-
-        var $mast = $('<div></div>');
-        $mast.addClass('view_mast');
-
-        var $img_content = $('<div></div>')
-        $img_content.attr('id', 'img_content');
-        $img_content.addClass('mast_pic')
-
-        var $img = $('<img/>');
-        var url = $(this).attr('src');
-        $img.attr('id', 'vier_img');
-        $img.attr('src', url);
-
-        var $prev = $('<span></span>');
-        $prev.addClass('prev iconfont icon-fanhui');
-        $img_content.append($prev);
-
-        var $next = $('<span></span>');
-        $next.addClass('next iconfont icon-jiantouyou');
-        $img_content.append($next);
-
-        var $actic = $('<p></p>');
-        var $img_alt = $(this).attr('alt');
-        $actic.addClass('actic');
-        $actic.html($img_alt);
-
-        var $reset = $('<span></span>');
-        $reset.addClass('reset');
-
-        $img_content.append($img);
-        $img_content.append($actic);
-        $img_content.append($reset);
-        $container.append($mast);
-        $container.append($img_content);
-        $('body').append($container);
-        resetPic(0.5);
-        hidePic();
-    });
-
-    function resetPic(scale) {
-        var $img_content = $('#img_content');
-        var $icon = $('.iconfont');
-        var $img = $('#vier_img');
-
-        var $i_width = $img.width() * scale;
-        var $i_height = $img.height() * scale;
-
-        $img_content.width(parseInt($i_width));
-        $img.width($img_content.width())
-        $icon.height($img.height());
-        $img_content.css("margin-top", -parseInt($i_height / 2));
-        $img_content.css("margin-left", -parseInt($i_width / 2));
-    }
-
-    function hidePic() {
-        $(".reset").click(function() {
-            $("#view-box").remove();
-        });
-    }
+    var vier = new Viewer();
 })
+
+function Viewer(obj) {
+    //this.a = obj.a;
+    //this.b = obj.b;
+    this.init();
+}
+Viewer.prototype.init = function() {
+    this.create();
+    this.addEven();
+    this.hidePic();
+}
+Viewer.prototype.create = function() {
+    var $container = $('<div></div>');
+    $container.attr('id', 'view-box');
+    $container.css('display', 'none');
+
+    var $mast = $('<div></div>');
+    $mast.addClass('view_mast');
+
+    var $img_content = $('<div></div>')
+    $img_content.attr('id', 'img_content');
+    $img_content.addClass('mast_pic')
+
+    var $img = $('<img/>');
+    $img.attr('id', 'vier_img');
+
+    var $prev = $('<span></span>');
+    $prev.addClass('prev iconfont icon-fanhui');
+    $img_content.append($prev);
+
+    var $next = $('<span></span>');
+    $next.addClass('next iconfont icon-jiantouyou');
+    $img_content.append($next);
+
+    var $actic = $('<p></p>');
+    $actic.addClass('actic');
+
+    var $reset = $('<span></span>');
+    $reset.addClass('reset');
+
+    $img_content.append($img);
+    $img_content.append($actic);
+    $img_content.append($reset);
+    $container.append($mast);
+    $container.append($img_content);
+    $('body').append($container);
+}
+Viewer.prototype.addEven = function() {
+    var _this = this;
+    $('body').on('click', '.view-pic', function() {
+        var $containe = $('#view-box'),
+            $img = $('#vier_img'),
+            $actic = $('.actic');
+        $containe.css('display', 'block');
+        var $url = $(this).attr('src');
+        $img.attr('src', $url);
+        var $img_alt = $(this).attr('alt');
+        $actic.html($img_alt);
+        _this.setSize(this, 0.5);
+    })
+}
+
+Viewer.prototype.setSize = function(img, scale) {
+    var $win_w = $(window).width(),
+        $win_h = $(window).height(),
+        $img_w = img.naturalWidth * scale,
+        $img_h = img.naturalHeight * scale;
+
+    var left = parseInt($win_w - $img_w) / 2,
+        top = parseInt($win_h - $img_h) / 2;
+
+    $('#vier_img').css({ 'width': $img_w, 'height': $img_h });
+    $('#img_content').css({ 'top': top, 'left': left });
+    $('.iconfont').css('height', $img_h);
+
+
+}
+Viewer.prototype.hidePic = function() {
+    $(".reset").on('click', function() {
+        $("#view-box").css('display', 'none');
+    });
+}
